565dc2424035ab8a16e2801947aff111b10fe08f
monishmohanty <monimohanty19@gmail.com>
2023-12-15 22:06:30 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'imageDetection',
+    version= '0.0.0',
+    author= 'Monish Mohanty',
+    author_email= 'monimohanty19@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
